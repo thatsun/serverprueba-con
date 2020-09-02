71b46f3546663e62d48babdd6b37835efb5ecd31
ramon_ramirez <marluxia122@hotmail.com>
2020-09-02 02:48:20 -0600

    una coma demas en el modelo de userroll fixed

--- api/models/userroll.js
+++ api/models/userroll.js
@@ -9,7 +9,7 @@ const userRollSchema = mongoose.Schema({
     },
     password: { type: String , require: true },
     username: { type: String , require: true },
-    userroll: { type: String , require: true },
+    userroll: { type: String , require: true }
 });
 
 module.exports= mongoose.model('Userroll', userRollSchema);
