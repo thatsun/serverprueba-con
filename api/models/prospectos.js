596ac0808add7d9345f0acc38e051f65450e7823
ramon_ramirez <marluxia122@hotmail.com>
2020-09-03 13:55:36 -0600

    ajustment

--- api/models/prospectos.js
+++ api/models/prospectos.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const prospectoSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Userroll', require: true},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
     name: { type: String , require: true },
     lastname: { type: String , require: true },
     lastlastname: { type: String , require: false },
