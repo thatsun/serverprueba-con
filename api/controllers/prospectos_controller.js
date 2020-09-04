a80dc365e8242da49e39ff13c6319fc80854b95c
ramon_ramirez <marluxia122@hotmail.com>
2020-09-03 18:11:34 -0600

    check test3

--- api/controllers/prospectos_controller.js
+++ api/controllers/prospectos_controller.js
@@ -127,7 +127,7 @@ exports.prospectos_add_prospecto=(req,res,next) =>{
                 phonenumber: req.body.phonenumber,
                 rfc: req.body.rfc,
                 documentsnames: ["",""],
-                documentsrequest: Array.from(req.files).map(file=>{ return file.destination + file.name }),
+                documentsrequest: ["",""],
                 status:req.body.status,
                 reject_details:""
         
