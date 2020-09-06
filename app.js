49488007d882a88fa64e1525c9a46b5097d98292
ramon_ramirez <marluxia122@hotmail.com>
2020-09-06 17:15:15 -0600

    seting up a uploads folder

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const docsRoutes= require('./api/routes/documentation');
 
 
 
-app.use(express.static(path.join(__dirname, "/uploads")));
+app.use('/uploads',express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(favicon(path.join(__dirname, 'public','favicon.ico')));
 
