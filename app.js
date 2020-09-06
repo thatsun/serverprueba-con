39a4c8a7b5915dc725a535afa4bfbb4f66c014a9
ramon_ramirez <marluxia122@hotmail.com>
2020-09-06 17:07:38 -0600

    jesus crist

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const docsRoutes= require('./api/routes/documentation');
 
 
 
-app.use(express.static('/uploads',express.static(process.env.PWD+'/uploads')));
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(path.join(__dirname, "/uploads")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(favicon(path.join(__dirname, 'public','favicon.ico')));
 
 
