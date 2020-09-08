13491e5786ebec2ad0d089f0c3254c8474ab6857
ramon_ramirez <marluxia122@hotmail.com>
2020-09-07 22:36:47 -0600

    cors fix

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/uploads',express.static('uploads'));
 
 app.use((req,res,next)=>{
     res.header("Acces-Control-Allow-Origin","*");
-    res.header("Acces-Control-Allow-Headres","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Acces-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if(req.method === 'OPTIONS'){
         res.header('Acces-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
